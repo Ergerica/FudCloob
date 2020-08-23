4f4dc38d17da51b411b4389ce7487e947c5629af
ergerica <ergerica76@gmail.com>
2020-08-22 22:55:57 -0400

    Initial Commit

---
+++ foodclub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodclub
+//
+//  Created by Erica Ross on 8/22/20.
+//  Copyright © 2020 Erica Ross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
